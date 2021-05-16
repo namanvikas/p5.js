e6d7c572994bdddf851e6666bd7a68dd6f8e635f
namanvikas <79072465+namanvikas@users.noreply.github.com>
2021-05-16 14:43:10 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -8,10 +8,19 @@ function setup(){
     video.hide();
 }
 function draw(){
-    image(video,0,0,640,480);
-    rect(0,0,55,55);
-    ellipse(,46,55,55);
-    circle(30,30,20)
+    image(video,230,150,220,200);
+    fill(255,0,0)
+    
+    circle(30,30,20);
+    circle(30,400,20);
+    circle(590,30,20);
+    circle(590,400,20);
+fill(0,255,0);
+rect(40,20,540,20);
+rect(20,40,20,350);
+rect(40,390,540,20);
+rect(580,40,20,350);
+
 }
 function take_snapshot(){
     save("supa strikas.jpg");
